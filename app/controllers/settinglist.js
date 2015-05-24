711a98bc476ae88b4014660e4f693994f2cb9ca2
dongjiawei <siroccoicode@gmail.com>
2015-05-25 00:48:21 +0800

    add feature

--- app/controllers/settinglist.js
+++ app/controllers/settinglist.js
@@ -1,1 +1,12 @@
 var args = arguments[0] || {};
+
+
+function isLoadPic(e){
+	var value=e.source.value;
+	alert(value);
+	if (value) {
+
+	}else{
+
+	}
+}
--- app/lib/mhjLib.js
+++ app/lib/mhjLib.js
@@ -15,3 +15,42 @@ exports.switchView=function(parent,views,index,preIndex){
 	}
 	
 };
+
+exports.isLoadPic=function(){
+	var isLoadPic=Ti.APP.Properties.getBool(Alloy.CFG.PLKEYS.LOADPIC,true);
+	return isLoadPic;
+}
+
+exports.isLogin=function(){
+	var userInfo=Ti.APP.Properties.getObject(Alloy.CFG.PLKEYS.USERINFO,null);
+	Ti.API.info("userInfo",userInfo);
+	if (userInfo != null) {
+		return true;
+	}else{
+		return false;
+	}
+}
+
+exports.isAppCheck=function(){
+	return Ti.App.Properties.getBool(Alloy.CFG.PLKEYS.APPCHECKED,false);
+}
+
+exports.getUserInfo=function(key){
+	var userInfo=Ti.APP.Properties.getObject(Alloy.CFG.PLKEYS.USERINFO,null);
+	Ti.API.info("userInfo",userInfo);
+	if (userInfo != null) {
+		return userInfo.key;
+	};
+}
+
+exports.getExtUserInfo=function(key){
+	var extUserInfo=Ti.APP.Properties.getObject(Alloy.CFG.PLKEYS.EXTUSERINFO,null);
+	Ti.API.info("userInfo",extUserInfo);
+	if (extUserInfo != null) {
+		return extUserInfo.key;
+	};
+}
+
+
+
+
